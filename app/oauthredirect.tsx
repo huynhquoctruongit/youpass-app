475d341ca53b12ab1c4043d8fb0a274d77f8ea30
truonghq <huynhquoctruongit@gmail.com>
2026-05-21 15:18:15 +0700

    update env

--- app/oauthredirect.tsx
+++ app/oauthredirect.tsx
@@ -80,9 +80,9 @@ export default function OAuthRedirect() {
     run();
   }, [params, router, loginWithGoogle]);
   useEffect(() => {
+
     const getParam: any = params
     loginWithGoogle(getParam.code);
-    
   }, []);
 
   return (
